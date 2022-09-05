b92b0d8774336ca1a93e19d20c4c086d9b90191b
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-09-05 15:56:47 -0400

    Update TweetTest.java

--- src/test/java/com/juggernauts/todoapp/API_Test/TweetTest.java
+++ src/test/java/com/juggernauts/todoapp/API_Test/TweetTest.java
@@ -28,21 +28,4 @@ class TweetTest {
 
     }
 
-    /**
-     * Method under test: {@link Tweet#shareApp(String)}
-     */
-    
-    @Test
-    @Disabled("TODO: Complete this test")
-    void testShareApp() {
-        // TODO: Complete this test.
-        //   Reason: R011 Sandboxing policy violation.
-        //   Diffblue Cover ran code in your project that tried
-        //     to access the network.
-        //   Diffblue Cover's default sandboxing policy disallows this in order to prevent
-        //   your code from damaging your system environment.
-        //   See https://diff.blue/R011 to resolve this issue.
-
-        (new Tweet()).shareApp("Tw");
-    }
 }
