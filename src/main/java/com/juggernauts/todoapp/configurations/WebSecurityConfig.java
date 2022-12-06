3ae913efd0d4f115bbb362ab8914be7e44cde9af
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-12-05 19:08:37 -0500

    Update WebSecurityConfig.java

--- src/main/java/com/juggernauts/todoapp/configurations/WebSecurityConfig.java
+++ src/main/java/com/juggernauts/todoapp/configurations/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig {
      */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
         http
             // setup /logout endpoint
             .logout().invalidateHttpSession(true).logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()).and()
